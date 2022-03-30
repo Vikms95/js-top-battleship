3407ce02fde4b50f8919770ea3f479ac9830d7d0
Vikms <vms1995@hotmail.com>
2022-03-30 17:55:51 +0200

    Add babel for jest

--- src/logic/ship.unit.test.js
+++ src/logic/ship.unit.test.js
@@ -1,9 +1,7 @@
 const ship = require('./ship')
 
 test('ship gets created', () =>{
-    expect(ship.Ship(2,'A2','A3')).toBe({
-        hit: function hit(){},
-        isSunk: function isSunk(){}
+    expect().toBe({
     })
 })
 
