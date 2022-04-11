eae535746b703e68bdf938fa05fb0ac21a57dd1d
Vikms <vms1995@hotmail.com>
2022-04-11 13:10:34 +0200

    Refactor Game to have properties outside of while loop

--- src/logic/game.js
+++ src/logic/game.js
@@ -2,38 +2,38 @@ import { Player } from './player'
 
 export function Game (){
 
-    const gameLoop = () =>{
-        
-        const player1 = Player('Victor')
-        player1.createGameBoard(
-            ['A1'],
-            ['B1','B2'],
-            ['C1','C2','C3'],
-            ['D1','D2','D3','D4'],
-            ['E1','E2','E3','E4','E5']
-        )
-        const player2 = Player('Olga')
-        player2.createGameBoard(
-            ['G8'],
-            ['B1','B2'],
-            ['C1','C2','C3'],
-            ['D1','D2','D3','D4'],
-            ['E1','E2','E3','E4','E5']
-        )
+    const player1 = Player('Victor')
+    player1.createGameBoard(
+        ['A1'],
+        ['B1','B2'],
+        ['C1','C2','C3'],
+        ['D1','D2','D3','D4'],
+        ['E1','E2','E3','E4','E5']
+    )
+    const player2 = Player('Olga')
+    player2.createGameBoard(
+        ['G8'],
+        ['B1','B2'],
+        ['C1','C2','C3'],
+        ['D1','D2','D3','D4'],
+        ['E1','E2','E3','E4','E5']
+    )
 
-        let gameboard1 = player1.getGameboard()
-        let gameboard2 = player2.getGameboard()
-        let playerInTurn = player2
-        let enemyGameboard = gameboard1
-        
+    let gameboard1 = player1.getGameboard()
+    let gameboard2 = player2.getGameboard()
+    let playerInTurn = player2
+    let enemyGameboard = gameboard1
+
+    const gameLoop = () =>{
+              
         // Testing variables
         let index = 0
         let coords
         let attack
 
-        // TODO  USE A 'DO WHILE LOOP'
         while(!player1.isPlayerDefeated()||!player2.isPlayerDefeated())
         {   
+            // Add the incremental index as testing
             coords = Object.keys(enemyGameboard.getBoardGrid())[index]
             // Testing guard clause to avoid error
             if(coords === undefined){
@@ -42,18 +42,19 @@ export function Game (){
             attack = playerInTurn.sendAttackCoordsToGame(coords)
             enemyGameboard.receiveAttackFromPlayer(attack)
             console.log(enemyGameboard.getBoardGrid())
-            playerInTurn = switchPlayers(player1,player2,playerInTurn)
-            enemyGameboard = switchGameboards(gameboard1,gameboard2,playerInTurn,player2)
+            playerInTurn = switchPlayers()
+            enemyGameboard = switchGameboards()
+            // Testing incremental index to auto attack
             index++
         }
     }
 
-    const switchPlayers = (p1,p2,pt) =>{
-        return pt === p2 ? p1 : p2
+    const switchPlayers = () =>{
+        return playerInTurn === player2 ? player1 : player2
     }
 
-    const switchGameboards = (gb1,gb2,pt,p2) =>{
-        return pt === p2 ? gb1 : gb2
+    const switchGameboards = () =>{
+        return playerInTurn === player2 ? gameboard1 : gameboard2
     }   
 
     return{gameLoop}
