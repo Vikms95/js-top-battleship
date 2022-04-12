3c254c4b151b2bb7b47011c98edbdd5cb56eadd3
Vikms <vms1995@hotmail.com>
2022-04-12 11:48:56 +0200

    Create view rendering module functions

---
+++ src/view/renderDynamicElements.js
@@ -0,0 +1,29 @@
+export function renderDynamicElements (attackIsSuccess, element){
+    attackIsSuccess ? renderSquareOnHit(element) : renderSquareOnMiss(element) 
+}
+
+const renderSquareOnHit = (element) =>{
+    element.classList.remove('ship')
+    element.classList.add('hit')
+}
+  
+const renderSquareOnMiss  = (element) =>{
+    element.classList.add('miss')
+  
+}
+
+const renderShipOnSink = () =>{
+
+}
+
+const renderMatchInfo = () =>{
+
+}
+
+const renderTurnInfo = () =>{
+  
+}
+
+const renderWarningsInfo = () =>{
+
+}
