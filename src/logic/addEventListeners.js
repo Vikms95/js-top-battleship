795ed4dec6f0c7389211ccb776e257cc5c13193e
Vikms95 <vms1995@hotmail.com>
2022-04-14 03:19:32 +0200

    Refactor renderDynamicElements functions

--- src/logic/addEventListeners.js
+++ src/logic/addEventListeners.js
@@ -1,30 +1,11 @@
-import {renderDynamicElements} from '../view/renderDynamicElements'
+import {renderTurn} from '../view/renderDynamicElements'
 
 
 export function addEventListeners (game){
     const gridSquaresComputer = Array.from(document.querySelectorAll('.player2 > .grid-square'))
-    gridSquaresComputer.forEach(square=>{
+    gridSquaresComputer.forEach(square =>{
         square.addEventListener('click',(event) =>{
-            const turnData = game.gameTurn(event.target.id)
-            if(turnData === null) return null
-
-            const {dataPlayer,dataComputer,attackedElement} = retrieveTurnData(turnData)
-            console.log(dataPlayer)
-            renderDynamicElements(dataPlayer, event.target)
-            renderDynamicElements(dataComputer, attackedElement)
+            renderTurn(game,event)
         })
     })
-}
-
-const findHitElement = (coords) =>{
-    return document.querySelector(`.player1 > #${coords}`)
-}
-
-const retrieveTurnData = (turnData) =>{
-    const dataPlayer = turnData.isPlayerAttackMiss
-    const dataComputer = turnData.isComputerAttackMiss
-    const computerCoords = turnData.computerCoords
-    const attackedElement = findHitElement(computerCoords)
-
-    return {dataPlayer,dataComputer,attackedElement}
 }
--- src/view/renderDynamicElements.js
+++ src/view/renderDynamicElements.js
@@ -1,12 +1,34 @@
-export function renderDynamicElements (turnData, element){
-    if(turnData){
-        if(element && element.classList.contains('hit')){
-            return 
-        }else{
-            renderSquareOnMiss(element)
-        }
-    }
-    renderSquareOnHit(element)
+export function renderTurn (game,event){
+    const turnData = game.gameTurn(event.target.id)
+    if(turnData === null) return
+
+    const {playerData,computerData, attackedElement} = 
+        retrieveTurnData(turnData)
+        
+    renderBoardSquares(playerData, event.target)
+    renderBoardSquares(computerData, attackedElement)
+}
+
+const retrieveTurnData = (turnData) =>{
+    const playerData      = turnData.isPlayerAttackMiss
+    const computerData    = turnData.isComputerAttackMiss
+    const computerCoords  = turnData.computerCoords
+    const attackedElement = findHitElement(computerCoords)
+
+    return { playerData,computerData, attackedElement }
+}
+
+const findHitElement = (coords) =>{
+    return document.querySelector(`.player1 > #${coords}`)
+}
+
+const renderBoardSquares = (turnData, element) =>{
+    if(turnData && isHitElement(element)) return
+    turnData ? renderSquareOnMiss(element) : renderSquareOnHit(element)
+}
+
+const isHitElement = (element) =>{
+    return element && element.classList.contains('hit') 
 }
 
 const renderSquareOnHit = (element) =>{
@@ -16,7 +38,6 @@ const renderSquareOnHit = (element) =>{
   
 const renderSquareOnMiss  = (element) =>{
     element.classList.add('miss')
-  
 }
 
 const renderShipOnSink = () =>{
